66160beff77419ae6bce7a4e411d863adc3b2a36
Suresh D <suresh.nashi10@gmail.com>
2026-03-13 17:56:39 +0530

    added map in contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Link } from "react-router-dom";
 import { ChevronRight } from "lucide-react";
+import Map from '@/assets/map.webp'
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -34,7 +35,7 @@ const Contact = () => {
       <Header />
 
       {/* Hero Section */}
-       <section className="relative h-[420px] overflow-hidden">
+      <section className="relative h-[420px] overflow-hidden">
         <img
           src={aboutHeroImg}
           alt="contact"
@@ -241,15 +242,26 @@ const Contact = () => {
       </section>
 
       {/* Map */}
-      <section className="max-w-screen-xl mx-auto pb-24 px-4 lg:px-0">
-        <div>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14961.623583430777!2d80.20002790961729!3d13.023714553345368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52672aca3b1a91%3A0x2532a78faa274768!2sSenlogic%20Automation%20Private%20Limited%2C%20Chennai!5e1!3m2!1sen!2sin!4v1773144886758!5m2!1sen!2sin"
-            className="border-0 w-full h-[500px]"
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+      <section className="xl:max-w-screen-xl max-w-full mx-auto pb-24 px-4 xl:px-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <div>
+            <h3 className="text-xl lg:text-3xl font-semibold mb-6">
+                Our Service Locations
+              </h3>
+              <img src={Map} alt="map" />
+          </div>
+          <div>
+            <h3 className="text-xl lg:text-3xl font-semibold mb-6">
+                Our Head Office
+              </h3>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14961.623583430777!2d80.20002790961729!3d13.023714553345368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52672aca3b1a91%3A0x2532a78faa274768!2sSenlogic%20Automation%20Private%20Limited%2C%20Chennai!5e1!3m2!1sen!2sin!4v1773144886758!5m2!1sen!2sin"
+              className="border-0 w-full xl:h-[600px] lg:h-[450px] md:h-[340px] h-[400px]"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </section>
 
