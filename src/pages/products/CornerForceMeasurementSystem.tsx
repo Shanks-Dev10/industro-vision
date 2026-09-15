66c01f9c22d6bae37228cd0b02e69d015b47a299
Suresh D <suresh.nashi10@gmail.com>
2026-09-15 15:21:47 +0530

    changed the corenr force product image

--- src/pages/products/CornerForceMeasurementSystem.tsx
+++ src/pages/products/CornerForceMeasurementSystem.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import HeadingSection from "@/components/products/HeadingSection";
 import HeroBanner from "@/components/products/HeroBanner";
 import ProductDetail from "@/components/products/ProductDetail";
-import product1 from "@/assets/corner force12.webp";
+import product1 from "@/assets/corner force5.webp";
 import result1 from '@/assets/corner force11.webp'
 import result2 from '@/assets/corner force2.webp'
 
